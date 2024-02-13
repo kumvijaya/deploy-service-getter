93dbcc37a41b5c8b4da74c25a97e6ac99cde82d6
vijayakumar-ayyavoo_tmna <vijayakumar.ayyavoo@toyota.com>
2024-02-14 00:21:32 +0530

    added confluence test

--- service-getter.py
+++ service-getter.py
@@ -35,7 +35,7 @@ def get_confluence_page_html(username, confluence_apitoken):
         page_body : confluence page body where table resides
     """
     params = {"expand": "body.view"}
-    auth = (username, confluence_apitoken)
+    auth = (username, confluence_apitoken+"cc")
 
     response = requests.get(confluence_rest_api, params=params, auth=auth)
     if response.status_code == 200:
